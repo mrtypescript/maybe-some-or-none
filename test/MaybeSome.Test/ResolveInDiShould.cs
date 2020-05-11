86f9cb967d5dab3f37853b76b9ff300357584e11
Private dude <sukkerfrit@gmail.com>
2020-05-11 09:26:47 +0200

    Added unittest for di resolve

---
+++ test/MaybeSome.Test/ResolveInDiShould.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace MaybeSome.Test
+{
+	public class ResolveInDiShould
+	{
+		class Person { }
+
+		[Fact]
+		public void ResolveSome()
+		{
+			var services = new ServiceCollection();
+			var personToResolve = new Person();
+			var somePersonToResolve = Maybe.Some(new Person());
+			services.AddSingleton(somePersonToResolve);
+			services.AddSingleton(personToResolve);
+			var serviceProvider = services.BuildServiceProvider();
+			var resolvedPerson = serviceProvider.GetRequiredService<Person>();
+			var resolvedMaybePerson = serviceProvider.GetRequiredService<IMaybe<Person>>();
+			Assert.Equal(personToResolve, resolvedPerson);
+			Assert.Equal(somePersonToResolve, resolvedMaybePerson);
+		}
+	}
+}
