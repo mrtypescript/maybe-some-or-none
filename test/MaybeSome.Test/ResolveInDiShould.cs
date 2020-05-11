4b99f22ee93244573e04b2e138e99c3e0ee9998a
Private dude <sukkerfrit@gmail.com>
2020-05-11 11:34:55 +0200

    Added test for resolving none and some

--- test/MaybeSome.Test/ResolveInDiShould.cs
+++ test/MaybeSome.Test/ResolveInDiShould.cs
@@ -5,21 +5,55 @@ namespace MaybeSome.Test
 {
 	public class ResolveInDiShould
 	{
+		class House
+		{
+			private readonly IMaybe<Person> _person;
+
+			public House(IMaybe<Person> person)
+			{
+				_person = person;
+			}
+		}
 		class Person { }
 
 		[Fact]
 		public void ResolveSome()
 		{
 			var services = new ServiceCollection();
+
 			var personToResolve = new Person();
 			var somePersonToResolve = Maybe.Some(new Person());
+
+			services.AddSingleton<House>();
+
 			services.AddSingleton(somePersonToResolve);
 			services.AddSingleton(personToResolve);
+
 			var serviceProvider = services.BuildServiceProvider();
+
 			var resolvedPerson = serviceProvider.GetRequiredService<Person>();
 			var resolvedMaybePerson = serviceProvider.GetRequiredService<IMaybe<Person>>();
+
+			var resolvedHouse = serviceProvider.GetRequiredService<House>();
+
 			Assert.Equal(personToResolve, resolvedPerson);
 			Assert.Equal(somePersonToResolve, resolvedMaybePerson);
+
+			Assert.NotNull(resolvedHouse);
+		}
+
+		[Fact]
+		public void ResolveNone()
+		{
+			var services = new ServiceCollection();
+
+			services.AddSingleton(Maybe.None<Person>());
+
+			var sp = services.BuildServiceProvider();
+
+			var resolvedNonePerson = sp.GetRequiredService<IMaybe<Person>>();
+
+			Assert.False(resolvedNonePerson.HasValue);
 		}
 	}
 }
